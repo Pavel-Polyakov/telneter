824bca1c3799197fddafa27192d846131e566744
laneus <polyakov.pavel.xyz@gmail.com>
2016-11-10 00:58:41 +0300

    some visual improvments and added useful methods

--- telneter.py
+++ telneter.py
@@ -9,7 +9,8 @@ class Account(object):
         self.password = password or getpass.getpass()
 
     def __repr__(self):
-        return "Account (username={})".format(self.username)
+        return 'Account (username="{}", password="{}")'.\
+                format(self.username, '*'*len(self.password))
 
 
 class Executor(object):
@@ -36,13 +37,12 @@ class Executor(object):
     def connect(self):
         self._tn = telnetlib.Telnet(self.hostname)
         self._tn.read_until(self.prompt_login, 10)
-        self._tn.write((self.account.username+"\n").encode('utf8'))
-        self._tn.expect([self.prompt_password], 10)
-        self._tn.write((self.account.password+"\n").encode('utf8'))
-        response = self._tn.expect([self.prompt_id_extreme,       # 0
-                                    self.prompt_id_juniper,       # 1
-                                    self.prompt_failed_login,     # 2
-                                    ]+self.prompt_all, 10)
+        self.run_and_expect(self.account.username, self.prompt_password, 10)
+        response = self.run_and_expect(self.account.password,
+                                       [self.prompt_id_extreme,
+                                        self.prompt_id_juniper,
+                                        self.prompt_failed_login] + self.prompt_all,
+                                       10)
         if response[0] == 0:
             self.__initialize_exos()
         elif response[0] == 1:
@@ -51,8 +51,8 @@ class Executor(object):
             self.close()
             raise ValueError("Failed login")
         else:
-            self._tn.write(b"show version\r\n")
-            response = self._tn.expect([b"(M|m)ore"]+self.prompt_all, 10)
+            response = self.run_and_expect("show version",
+                                           [b"(M|m)ore"]+self.prompt_all, 10)
             if b"Cisco IOS" in response[-1]:
                 self.__initialize_ios()
             else:
@@ -67,20 +67,21 @@ class Executor(object):
     def __initialize_ios(self):
         self.connected = True
         self.os = "IOS"
-        self._tn.write(b"\x03")
-        self._tn.write(b"terminal length 0\r\n")
+        self.ctrlc()
+        self.run("terminal length 0")
 
     def __initialize_exos(self):
         self.connected = True
         self.os = "EXOS"
-        self._tn.write(b"disable clipaging\r\n")
+        self.run('disable clipaging')
+        time.sleep(1)
 
     def __initialize_junos(self):
         self.connected = True
         self.os = "JUNOS"
-        self._tn.write(self.eol)
-        self._tn.write(b"cli\r\n")
-        self._tn.write(b"set cli screen-length 0\r\n")
+        self.run(self.eol)
+        self.run("cli")
+        self.run("set cli screen-length 0")
 
     def cmd(self, command):
         if self.os == "JUNOS":
@@ -93,11 +94,43 @@ class Executor(object):
             prompt = self.prompt_all
 
         time.sleep(1)
-        self._tn.read_very_eager()
-        self._tn.write(command.encode('utf8')+self.eol)
-        output = self._tn.expect(prompt, 30)[-1]
+        self.read_all()
+        output = self.run_and_expect(command, prompt)[-1]
         return output.decode('utf8', errors='replace')
 
+    def expect(self, expect, timer=30):
+        if isinstance(expect, list):
+            cleaned = [self.__convert_to_bytes(x) for x in expect]
+        else:
+            cleaned = [self.__convert_to_bytes(expect)]
+        return self._tn.expect(cleaned, timer)
+
+    def run(self, command):
+        self._tn.write(self.__convert_to_bytes(command)+self.eol)
+
+    def run_and_expect(self, command, expect, timer=30):
+        self.run(command)
+        return self.expect(expect, timer)
+
+    def read_all(self):
+        return self._tn.read_very_eager()
+
+    def ctrlc(self):
+        self.run(b"\x03")
+
+    @staticmethod
+    def __convert_to_bytes(value):
+        result = None
+        if isinstance(value, str):
+            result = value.encode('utf8')
+        elif isinstance(value, (float, int, )):
+            result = str(value).encode('utf8')
+        elif isinstance(value, bytes):
+            result = value
+        else:
+            result = bytes(value)
+        return result
+
     def __repr__(self):
-        return "Executor (hostname={},os={},connected={})".\
+        return 'Executor (hostname="{}", os="{}", connected={})'.\
                 format(self.hostname, self.os, self.connected)
